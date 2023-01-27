0d3deee4d2ba17dfcc6707d6403a6dfe5ae1d9a0
ZyNordd <m2107337@edu.misis.ru>
2023-01-27 16:13:49 +0300

    fixes

--- parser.cpp
+++ parser.cpp
@@ -5,7 +5,11 @@
 #include <QObject>
 #include <iostream>
 
+
 int main(int argc, char* argv[]) {
+    //qDebug() << "SslSupport: " << QSslSocket::supportsSsl();
+    //qDebug() << "SslLibraryBuildVersion: " << QSslSocket::sslLibraryBuildVersionString();
+    //qDebug() << "SslLibraryRuntimeVersion: " << QSslSocket::sslLibraryVersionString();
     auto* a = new QApplication(argc, argv);
     MainWindow w;
     w.show();
